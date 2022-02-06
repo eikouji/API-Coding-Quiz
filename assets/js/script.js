44f33787bc9a7470c62491d2a2921fbf3acea1dc
Eiko Ujifusa <eikorouji@gmail.com>
2022-02-06 13:23:53 -0800

    created a To - Do function list, script.js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,35 @@
 
+var buttonE1 = document.querySelector("#ans-btn");
+
+// target the ul element in HTML section by class //
+var quizQuestionsE1 = document.querySelector("#question-list-ul")
+
+// Create answer option, style answer option, add text of answer option, 
+// append this element to the answer list of the question. //
+
+// function for quiz question // 
+var createQuizQuestion = function() {
+    var questionItemE1 = document.createElement("ul") {
+        questionItemE1.className = "question-list-ul";
+        questionItemE1.textContent = "";
+        quizQuestionsE1.appendChild(createQuizQuestion);
+    };
+
+};
+
+buttonE1.addEventListener("click", createTaskHandler);
+
+// function for quiz answers
+var answerOptions = function() {
+
+};
+
+
+// quiz timer function //
 var quizTimer = new Date().getTime();
 
-// Update the count down to every 1 second //
 
+// Update the count down to every 1 second //
 var x = setInterval(function() {
     // get today's date and time //
     var now = new Date().getTime();
@@ -15,5 +42,15 @@ var x = setInterval(function() {
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "Time's Up! Quiz Over!";
-    }
-} 5000);
+    };
+
+
+    /*
+    To Do:
+    1. Create quiz question card with dynamically created questions (question function)
+    2. Populate quiz with 4 answers per multiple choice question (answer function)
+    3. Tally right answers, ding time for wrong answers (tally, timer subtract function)
+    4. Timer function (set timer)
+    5. record name and end quiz score (localStorage feature)
+    */
+};
