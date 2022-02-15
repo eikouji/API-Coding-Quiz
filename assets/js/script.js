6eb76d872b0718fdc96d6229595dca7566c4298e
Eiko Ujifusa <eikorouji@gmail.com>
2022-02-14 22:33:37 -0800

    trying to get these files to work.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var quizQuestionsE1 = document.querySelector("#question-list-ul")
 // Create answer option, style answer option, add text of answer option, 
 // append this element to the answer list of the question. //
 
-// function for quiz question // 
+// function for quiz question presentation// 
 var createQuizQuestion = function() {
     var questionItemE1 = document.createElement("ul") {
         questionItemE1.className = "question-list-ul";
@@ -17,40 +17,37 @@ var createQuizQuestion = function() {
 
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+buttonE1.addEventListener("click", function() {
+    alert("button clicked");
+});
 
 // function for quiz answers
 var answerOptions = function() {
 
 };
 
-
-// quiz timer function //
-var quizTimer = new Date().getTime();
-
-
-// Update the count down to every 1 second //
-var x = setInterval(function() {
-    // get today's date and time //
-    var now = new Date().getTime();
-
-    // display result in the element with id "quiz-timer" //
-    document.getElementById("quiz-timer").innerHTML =
-    "you have" + minutes + "Minutes" + seconds + "seconds" + "left";
-
-    // if countdown is finished, write text //
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "Time's Up! Quiz Over!";
-    };
+// create the quiz function, with quiz question array injected onto/into HTML //
+createCodingQuiz();
 
 
-    /*
-    To Do:
-    1. Create quiz question card with dynamically created questions (question function)
-    2. Populate quiz with 4 answers per multiple choice question (answer function)
-    3. Tally right answers, ding time for wrong answers (tally, timer subtract function)
-    4. Timer function (set timer)
-    5. record name and end quiz score (localStorage feature)
-    */
-};
+// save quiz score // 
+var saveTasks = function() {
+    localStorage.setItem("quizQuestions", JSON.stringify(quizQuestions));
+}
+
+
+
+// Quiz questions and their answers //
+// Quiz Question 1, Array of answers //
+// Quiz Question 2, Array of Answers //
+// Quiz Question 3 - 10, Array of Answers // 
+
+
+
+/* OnClick function -- 
+On clicking Start Quiz button, the following needs to happen - 
+1. Timer starts from 15, counts down by 1 second to 0
+2. Question with 4 multiple choice answers appears. Each answer is click-able.
+3. With a right answer, a score is tallied. With a wrong answer, 3 seconds are subtracted from timer.
+4. When timer is done, or when questions are all answered, score is recorded for tracking progress.
+*/
