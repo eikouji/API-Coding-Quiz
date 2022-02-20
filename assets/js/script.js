ec572392172c4b8d165d111516e89fd72a97b840
Eiko Ujifusa <eikorouji@gmail.com>
2022-02-20 13:57:23 -0800

    different coding strategy for the .js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,99 @@
 
+// Var Array, Quiz Question Array //
 
-function Quiz(questions) {
-    this.score = 0;
-    this.questions = questions;
-    this.questionIndex = 0;
-}
+// 10 questions total //
+var quizQuestions = [
+    {
+        title: "How do you write a comment in a JavaScript file?",
+        choices: ["<!-- comment -->", "// comment //", "%% comment %%", "-- comment --"],
+        answer: "// comment //"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "Which of the options is the right way to call a random number?",
+        choices: ["var randomNum = Math.random();", "var randomNum = Random.math();", "math.number() = randomNum", "solve.random() = math",],
+        answer: "var randomNum = Math.random();"
+    },
+    {
+        title: "Which of the following function of Array Object removes the last element from an array and returns that element?",
+        choices: ["pop()", "push()", "join()", "map()",],
+        answer: "pop() -- removes the last element from an array and returns that element"
+    },
+    {
+        title: "How do you round the number 7.25 to the nearest integer?",
+        choices: ["Math.Round(7.25);", "round(7.25);", "math.compute(7.25);", "calc.math(7.25);",],
+        answer: "Math.Round(7.25)"
+    },
+    {
+        title: "Which of the following will result in a pop-up, prompting the user for information?",
+        choices: ["notifiy()", "yoohoo()", "alert()", "prompt()",],
+        answer: "prompt()"
+    },
+    {
+        title: "Which JavaScript element represents TRUE or FALSE values?",
+        choices: ["RegExp", "Array", "Boolean", "Event"],
+        answer: "Boolean"
+    },
+    {
+        title: "What is the object called that lets you work with both dates and time-related data?",
+        choices: ["Clock", "Time-field", "Time-Warp", "Dates"],
+        answer: ""
+    },
+    {
+        title: "What is the language or list of instructions that are executed by the computer (how JavaScript is built)?",
+        choices: ["Syntax", "JSON", "Scope", "Input"],
+        answer: ""
+    },
+    {
+        title: "A useful tool to checking your own code during development and the de-bugging process is:",
+        choices: ["checkCode()", "for loops", "console.log()", "debug.document"],
+        answer: "console.log()"
+    },
+];
 
-Quiz.prototype.getQuestionIndex = function() {
-    return this.questions[this.questionIndex];
-}
+// Quiz Variables // 
+var score = 0
+var questionsIndex = 0;
 
-// keeps score //
-Quiz.prototype.guess = function(answer) {
-    if(this.getQuestion().isCorrectAnswer(answer)) {
-        this.score++;
+// more Quiz variables //
+var currentTime = document.querySelector("#currentTime");
+var Timer = document.querySelector("#quiz-count-down-timer");
+var questionsDiv = document.querySelector("questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+
+// Seconds left is 15 seconds per question : 
+var secondsLeft = 76;
+
+// holds interval time //
+varHoldInterval = 0;
+
+// holds penalty time // 
+var holdPenalty = 10;
+
+// creates new element //
+var ulCreate = document.createElement("ul");
+
+// Clicking on Start Quiz button starts timer, Timer starts ticking // 
+timer.addEventListener("click", function() {
+    // checking zero //
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function() {
+            secondsLeft--;
+            currentTime.textContent = "Time" + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!"
+            }
+        }, 1000)
     }
+    render(questionsIndex)
 }
 
-Quiz.prototype.isEnded = function() {
-    return this.questionIndex === this.questions.length;
-}
 
-function Question(text, choices, answer) {
-    this.text = text;
-    this.choices = choices;
-    this.answer = answer;
-}
 
-Question.prototype.isCorrectAnswer = function(choice) {
-    return this.answer === choice;
-}
-
-function populate() {
-    if(quiz.isEnded()) {
-        showScores();
-    } else {
-        // show quiz question //
-        var element = document.getElementById("question");
-        element.innerHTML = quiz.getQuestionIndex().text;
-
-        // show answer options //
-        var Choices = quiz.getQuestionIndex.choices;
-        for(var i = 0; i < choices.length; i++) {
-            var element = document.getElementById("choice" + i);
-            element.innerHtml = choices[i];
-            guess("btn" + i, choices[i]);
-        }
-
-        showProgress();
-    }
-
-};
-
-function guess(id, guess) {
-    var button = document.getElementById(id);
-    button.onclick = function() {
-        quiz.guess(guess);
-        populate();
-    }
-};
-
-// current question, progress //
-function showProgress() {
-    var currentQuestionNumber = quiz.questionIndex + 1;
-    var element = document.getElementById("progress");
-    element.innerHTML = "Question" + currentQuestionNumber + " of " + quiz.questions.length;
-
-};
-
-function showScores() {
-    var gameOverHTML = "<h1>Result";
-    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-    var element = document.getElementById("quiz");
-    element.innerHTML = gameOverHTML;
-};
-
-// quiz questions //
-/* quiz questions */
-
-var questions = [
-    new Question ("How do you add comments to a JavaScript file?", ["// comment //", "<!-- comment -->"], ["&& comment %%"], ["@@ comment @@"]),
-    new Question ("Which one of these answers is a Conditional Expression?" ["Alternative to if-else"], ["Switch statement"], ["if-then-else"], ["immediate if"]),
-    new Question ("When you see 'function' and 'var' in a JavaScript file, these are:" ["Keywords"], ["Data Types"], ["Declaration Statements"], ["Protocols"]),
-    new Question ("True or false: It's a good idea to commit your code often, or whenever you leave your desk or workspace for an extended time.", ["True"], ["True"], ["True"], ["True"]),
-    new Question ("")
-    ])
-]
